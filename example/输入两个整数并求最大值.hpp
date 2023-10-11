d1cbca0607bcf115357656ff183331330dfd2cac
Ray <ray2007123@gmail.com>
2023-10-11 14:32:55 -0400

    update

---
+++ example/输入两个整数并求最大值.hpp
@@ -0,0 +1,13 @@
+#include <iostream.h>
+using namespace std;
+int max(int i, int j){  /*定义max()函数*/
+    if (i>=j) return i;
+    else      return j;
+}
+int main(void){  /*定义main()函数*/
+    cout<<"输入i,j：";  //显示提示信息
+    int i, j;  //说明变量
+    cin>>i>>j;  //从键盘上输入变量的值
+    cout<<"最大数是："<<max(i, j)<<'\n';  //输出提示信息和结果
+    return 0;
+}
